de7eaa9bae40f0ec24d64f80193d0a4afa7419d4
2012kh79 <2012kh79@gmail.com>
2020-06-04 20:02:27 +0300

    Провека наличия раздела в реестре

--- SiPPOON_PP/Forms/Form_Zastavka.cs
+++ SiPPOON_PP/Forms/Form_Zastavka.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Drawing;
 using System.Windows.Forms;
+using Microsoft.Win32;
 
 namespace SiPPOON_PP
 {
@@ -15,13 +16,22 @@ namespace SiPPOON_PP
 
         private void timer1_Tick(object sender, EventArgs e)
         {
+            RegistryKey registry = Registry.CurrentUser.OpenSubKey("Uchet_Rez");
             if (Opacity != 1)
                 Opacity += 0.01;
             else
             {
                 timer1.Enabled = false;
-                Forms.Form_Connection form_Connection = new Forms.Form_Connection();
-                form_Connection.Show();
+                if (registry != null)
+                {
+                    Form_Authorize form_Authorize = new Form_Authorize();
+                    form_Authorize.Show();
+                }
+                else
+                {
+                    Forms.Form_Connection form_Connection = new Forms.Form_Connection();
+                    form_Connection.Show();
+                }
                 this.Hide();
             }
         }
